22b15ee53601af19567d120a398dd6f8744be3ee
frgri <francois.grimm@yogarik.com>
2023-09-24 17:08:54 +0200

    first pass at picking characters

--- src/components/AdminTabPick.vue
+++ src/components/AdminTabPick.vue
@@ -1,20 +1,21 @@
 <script>
 import router, { usePlayerStore } from '../main';
-import { ref } from 'vue'
 import VueMultiselect from "vue-multiselect";
-import {Dataset, DatasetInfo, DatasetItem, DatasetSearch, DatasetShow} from "vue-dataset";
 import {useI18n} from "vue-i18n";
 
 export default {
   components: {
-
+    VueMultiselect
   },
   data() {
     const store = usePlayerStore();
     const { t } = useI18n();
+    const pick_multiselect = [];
+    const type_pick = 'all'
+    const nb_targets = 0;
 
     return {
-      store, t
+      store, t, pick_multiselect, type_pick, nb_targets
     }
   },
   mounted() {
@@ -29,10 +30,32 @@ export default {
 <template>
   <div class="tab" ref="tab">
     <div id='tab-pick-content'>
+      <input type="number" v-model="nb_targets">
+      <select v-model="type_pick">
+        <option value="all">Parmi les personnages ayant un de ces tags :</option>
+        <option value="all">Parmi les personnages ayant tous ces tags :</option>
+        <option value="each">Pour chaque tag ({{ nb_targets }} par tag) :</option>
+      </select>
+      <vue-multiselect
+          ref="pick_multiselect"
+          id="pick_multiselect"
+          v-model="pick_multiselect"
+          label="label"
+          track-by="code"
+          placeholder="Choisir un tag"
+          tagPlaceholder="Choisir un tag"
+          noOptions="Tout le monde"
+          :options=store.tags
+          :multiple="true"
+          :taggable="false"
+          :hideSelected="true"
+      ></vue-multiselect>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+  #tab-pick-content {
+    flex-direction: row;
+  }
 </style>
